b2d1c31cda832f5effe77748d8102e93157fc641
ex-xuxiaopeng001 <1609798079@qq.com>
2018-04-01 01:10:28 +0800

    软键盘挡住edittext

--- app/src/main/java/com/hh/gridview_recyclerview/View/DrawQQLayout.java
+++ app/src/main/java/com/hh/gridview_recyclerview/View/DrawQQLayout.java
@@ -23,15 +23,14 @@ public class DrawQQLayout extends FrameLayout {
 
     private ViewDragHelper.Callback cb;
     private ViewDragHelper viewDragHelper;
-    private ViewGroup leftMenu;
     private ViewGroup mainMenu;
 
     public DrawQQLayout(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public DrawQQLayout(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public DrawQQLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -40,7 +39,7 @@ public class DrawQQLayout extends FrameLayout {
     }
 
     private void init() {
-        cb = new ViewDragHelper.Callback(){
+        cb = new ViewDragHelper.Callback() {
 
             @Override
             public boolean tryCaptureView(View child, int pointerId) {
@@ -55,22 +54,27 @@ public class DrawQQLayout extends FrameLayout {
 
 
             @Override
+            public int clampViewPositionVertical(View child, int top, int dy) {
+                return fixLeft(top);
+            }
+
+            @Override
             public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
                 //当位置改变时候，处理要做的事（更新状态，伴随动画，重绘界面）
                 super.onViewPositionChanged(changedView, left, top, dx, dy);
-                int newleft = left;
-                if (changedView == leftMenu) {
-                    newleft = mainMenu.getLeft() + dx;
-                    LogUtil.d("DrawQQLayout", "newleft" + newleft + "dx" + dx + "mainMenu.getLeft()" + mainMenu.getLeft());
-
-                    newleft = fixLeft(newleft);
-                    leftMenu.layout(0, 0, mWidth, mHeigh);
-                    mainMenu.layout(0 + newleft, 0, mWidth + newleft, mHeigh);
-                }
-                //更新状态执行动画
-                dispatchDrawEvent(newleft);
-                //为了兼容低版本2.3以下，每次修改值后重绘
-                invalidate();
+//                int newleft = left;
+//                if (changedView == leftMenu) {
+//                    newleft = mainMenu.getLeft() + dx;
+//                    LogUtil.d("DrawQQLayout", "newleft" + newleft + "dx" + dx + "mainMenu.getLeft()" + mainMenu.getLeft());
+//
+//                    newleft = fixLeft(newleft);
+//                    leftMenu.layout(0, 0, mWidth, mHeigh);
+//                    mainMenu.layout(0 + newleft, 0, mWidth + newleft, mHeigh);
+//                }
+//                //更新状态执行动画
+//                dispatchDrawEvent(newleft);
+//                //为了兼容低版本2.3以下，每次修改值后重绘
+//                invalidate();
             }
 
 
@@ -81,13 +85,7 @@ public class DrawQQLayout extends FrameLayout {
                 //xvel 水平方向的速度
                 //yvel 垂直方向的速度
                 super.onViewReleased(releasedChild, xvel, yvel);
-                if (xvel == 0 && mainMenu.getLeft() > range / 2.0) {
-                    open();
-                } else if (xvel > 0) {
-                    open();
-                } else {
-                    close();
-                }
+                close();
             }
         };
 
@@ -95,25 +93,25 @@ public class DrawQQLayout extends FrameLayout {
 
     }
 
-    private void dispatchDrawEvent(int newleft) {
-        //伴随动画
-        float persent = newleft * 1.0f / range;
-        // 1、左面板：缩放动画，平移动画，透明度变化
-        //缩放动画 0.5 》》0.5 + 0.5 * peesent
-        leftMenu.setScaleX(0.5f + 0.5f * persent);
-        leftMenu.setScaleY(0.5f + 0.5f * persent);
-        //平移动画 -mWidth /2 >>> 0
-        leftMenu.setTranslationX( -mWidth / 2.0f + persent * (mWidth / 2) );
-        //透明度0.5》》1.0
-        leftMenu.setAlpha(0.5f + 0.5f * persent);
-
-
-        // 2、主面板：缩放动画
-        mainMenu.setScaleX((float) (1 - persent * 0.2));
-        mainMenu.setScaleY((float) (1 - persent * 0.2));
-
-
-    }
+//    private void dispatchDrawEvent(int newleft) {
+//        //伴随动画
+//        float persent = newleft * 1.0f / range;
+//        // 1、左面板：缩放动画，平移动画，透明度变化
+//        //缩放动画 0.5 》》0.5 + 0.5 * peesent
+//        leftMenu.setScaleX(0.5f + 0.5f * persent);
+//        leftMenu.setScaleY(0.5f + 0.5f * persent);
+//        //平移动画 -mWidth /2 >>> 0
+//        leftMenu.setTranslationX( -mWidth / 2.0f + persent * (mWidth / 2) );
+//        //透明度0.5》》1.0
+//        leftMenu.setAlpha(0.5f + 0.5f * persent);
+//
+//
+//        // 2、主面板：缩放动画
+//        mainMenu.setScaleX((float) (1 - persent * 0.2));
+//        mainMenu.setScaleY((float) (1 - persent * 0.2));
+//
+//
+//    }
 
     @Override
     public void computeScroll() {
@@ -130,6 +128,7 @@ public class DrawQQLayout extends FrameLayout {
     private void close() {
         close(true);
     }
+
     private void close(boolean smooth) {
         int finalLeft = 0;
         if (smooth) {
@@ -139,14 +138,15 @@ public class DrawQQLayout extends FrameLayout {
                 //参数传this（child所在的parent）
                 ViewCompat.postInvalidateOnAnimation(this);
             }
-        }else {
-            mainMenu.layout(0,0,mWidth,mHeigh);
+        } else {
+            mainMenu.layout(0, 0, mWidth, mHeigh);
         }
     }
 
     private void open() {
         open(true);
     }
+
     private void open(boolean smooth) {
         int finalLeft = range;
         if (smooth) {
@@ -156,27 +156,38 @@ public class DrawQQLayout extends FrameLayout {
                 //参数传this（child所在的parent）
                 ViewCompat.postInvalidateOnAnimation(this);
             }
-        }else{
-            mainMenu.layout(range,0,range+ mWidth,mHeigh);
+        } else {
+            mainMenu.layout(range, 0, range + mWidth, mHeigh);
         }
     }
 
-    private int fixLeft(int left) {
-        if (left < 0){
-            return 0;
-        } else if (left > range) {
-            return  range;
-        }
-        return left;
+    private int fixLeft(int top) {
+
+        return top;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        LogUtil.d("touch","dispatchTouchEvent");
+        return super.dispatchTouchEvent(ev);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return viewDragHelper.shouldInterceptTouchEvent(ev);
+        LogUtil.d("touch","onInterceptTouchEvent");
+
+        viewDragHelper.shouldInterceptTouchEvent(ev);
+        if (ev.equals(MotionEvent.ACTION_UP)) {
+            return false;
+        }else {
+            return true;
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        LogUtil.d("touch","onTouchEvent");
+
         viewDragHelper.processTouchEvent(event);
         return true;
     }
@@ -184,16 +195,15 @@ public class DrawQQLayout extends FrameLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        leftMenu = (ViewGroup) getChildAt(0);
-        mainMenu = (ViewGroup) getChildAt(1);
+        mainMenu = (ViewGroup) getChildAt(0);
 
-        if (!(leftMenu instanceof ViewGroup && mainMenu instanceof ViewGroup)) {
+        if (!(mainMenu instanceof ViewGroup)) {
 
             throw new RuntimeException("孩子不是ViewGroup");
         }
 
-        if (getChildCount() < 2) {
-            throw new RuntimeException("至少有两个子孩子");
+        if (getChildCount() < 1) {
+            throw new RuntimeException("至少有1个子孩子");
         }
     }
 
@@ -207,6 +217,6 @@ public class DrawQQLayout extends FrameLayout {
         super.onSizeChanged(w, h, oldw, oldh);
         mHeigh = getMeasuredHeight();
         mWidth = getMeasuredWidth();
-        range = (int) (0.6 * mWidth);
+        range = (int) (0.3 * mWidth);
     }
 }
--- app/src/main/java/com/hh/gridview_recyclerview/activity/SlideActivity.java
+++ app/src/main/java/com/hh/gridview_recyclerview/activity/SlideActivity.java
@@ -6,6 +6,7 @@ import android.os.HandlerThread;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.hh.gridview_recyclerview.R;
@@ -22,7 +23,7 @@ import com.hh.gridview_recyclerview.utils.LogUtil;
  https://www.cnblogs.com/zhaoyanjun/p/6062880.html
  */
 
-public class SlideActivity extends AppCompatActivity {
+public class SlideActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "DrawQQLayout";
     private TextView main_tv;
     private HandlerThread myHandlerThread ;
@@ -32,9 +33,10 @@ public class SlideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LogUtil.d(TAG,"onCreate");
-        setContentView(R.layout.activity_slide);
-        main_tv = (TextView) findViewById(R.id.main_tv);
 
+        setContentView(R.layout.activity_slides);
+        main_tv = (TextView) findViewById(R.id.main_tv);
+        main_tv.setOnClickListener(this);
 
         //创建一个线程,线程名字：handler-thread
         myHandlerThread = new HandlerThread( "handler-thread");
@@ -70,5 +72,10 @@ public class SlideActivity extends AppCompatActivity {
 
         //释放资源
         myHandlerThread.quit() ;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
